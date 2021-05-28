480d384ec8aeda52a852cd9995077bb3add87fdd
Ross Semenov <ross@tecton.ai>
2021-05-28 11:00:09 -0400

    use extra_pip_dependencies

--- ads/features/content_keyword_impression_counts.py
+++ ads/features/content_keyword_impression_counts.py
@@ -1,17 +1,24 @@
 from tecton.feature_views import stream_window_aggregate_feature_view
 from tecton.feature_views.feature_view import Input
-from tecton import FeatureAggregation
+from tecton import FeatureAggregation, NewDatabricksClusterConfig
 from ads.entities import content_keyword
 from ads.data_sources.ad_impressions_stream import ad_impressions_stream
 from datetime import datetime
 
+cluster_config = NewDatabricksClusterConfig(
+    instance_type='m4.xlarge',
+    number_of_workers=4,
+    extra_pip_dependencies=["tensorflow==2.2.0"],
+)
 
 @stream_window_aggregate_feature_view(
     inputs={'ad_impressions': Input(ad_impressions_stream)},
     entities=[content_keyword],
-    mode='spark_sql',
+    mode='pyspark',
     aggregation_slide_period='continuous', # enable low latency streaming
     aggregations=[FeatureAggregation(column='impression', function='count', time_windows=['1h', '12h', '24h','72h','168h'])],
+    batch_materialization=cluster_config,
+    streaming_materialization=cluster_config,
     online=True,
     offline=True,
     feature_start_time=datetime(2021, 1, 1),
@@ -21,11 +28,13 @@ from datetime import datetime
     description='The count of ad impressions for a content_keyword'
 )
 def content_keyword_impression_counts(ad_impressions):
-    return f'''
-        SELECT
-            content_keyword,
-            1 as impression,
-            timestamp
-        FROM
-            {ad_impressions}
-        '''
+    from pyspark.sql import functions as F
+    from pyspark.sql.functions import lit
+    from pyspark.sql.types import StringType
+
+    def lowercase(s):
+        import tensorflow as tf
+        return tf.strings.lower(s).numpy().decode('UTF-8')
+
+    lowercase_udf = F.udf(lowercase, StringType())
+    return ad_impressions.select(lowercase_udf('content_keyword').alias('content_keyword'), 'timestamp').withColumn('impression', lit(1))
