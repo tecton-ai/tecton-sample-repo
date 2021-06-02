0850d3d806d0e6b192190c93833833b5877feba0
Jack Wells <jack@tecton.ai>
2021-06-02 14:17:00 -0700

    Updates

--- fraud/features/tests/test_transaction_bucketing.py
+++ fraud/features/tests/test_transaction_bucketing.py
@@ -2,24 +2,18 @@ from fraud.features.transaction_bucketing import transaction_bucketing
 import pandas as pd
 from pandas.testing import assert_frame_equal
 
-# # Testing the 'transaction_amount_is_high' feature which depends on request data ('amount') as input
-# def test_transaction_amount_is_high():
-#     transaction_request = pd.DataFrame({'amount': [124, 10001, 34235436234]})
+# Testing the 'transaction_bucketing' feature which depends on request data as input
+def test_transaction_bucketing():
+    transaction_request = pd.DataFrame([
+        [15000, 0, 0, 0, 0, 1],
+        [5000, 1, 0, 0, 0, 0],
+        [40000, 0, 1, 0, 0, 0]
+    ], columns=['amount', 'type_CASH_IN', 'type_CASH_OUT', 'type_DEBIT', 'type_PAYMENT', 'type_TRANSFER'])
+    expected = pd.DataFrame([
+        [2, 'transfer'],
+        [0, 'credit'],
+        [4, 'debit']
+    ], columns=['amount_bucketed', 'type_bucketed'])
 
-#     actual = transaction_amount_is_high.run(transaction_request=transaction_request)
-#     expected = pd.DataFrame({'transaction_amount_is_high': [0, 1, 1]})
-
-#     assert_frame_equal(actual, expected)
-
-# def test_transaction_bucketing():
-#     transaction_request = pd.DataFrame([
-#         [15000, 0, 0, 0, 0, 1]
-#     ], columns=['amount', 'type_CASH_IN', 'type_CASH_OUT', 'type_DEBIT', 'type_PAYMENT', 'type_TRANSFER'])
-
-#     print(transaction_bucketing.inputs.keys())
-#     actual = transaction_bucketing.run(transaction_request=transaction_request)
-#     expected = pd.DataFrame({'transaction_bucketing': [2, 'transfer']})
-
-#     print(actual)
-
-#     assert_frame_equal(actual, expected)
+    actual = transaction_bucketing.run(transaction_request=transaction_request)
+    assert_frame_equal(actual, expected)
--- fraud/features/transaction_bucketing.py
+++ fraud/features/transaction_bucketing.py
@@ -18,37 +18,54 @@ output_schema = StructType([
     StructField('type_bucketed', StringType())
 ])
 
-# # Bucket the transaction amount by each $10,000 dollars
-# # This is a reusable transformation you can use in multiple feature views
+# Bucket the transaction amount by each $10,000 dollars
+# This is a reusable transformation you can use in multiple feature views
 @transformation(mode="pandas")
 def tx_amount_bucketed_transformation(tx: pandas.DataFrame):
-    import pandas
-    tx['amount_bucketed'] = (tx.amount / 10000).round().astype('long')
-    return tx
+    import pandas as pd
 
-# # Bucket the transaction type as credit, debit, or transfer
-# # This is a reusable transformation you can use in multiple feature views
+    df = pd.DataFrame()
+    df['amount_bucketed'] = (tx['amount'] / 10000).round().astype('long')
+    return df[['amount_bucketed']]
+
+# Bucket the transaction type as credit, debit, or transfer
+# This is a reusable transformation you can use in multiple feature views
 @transformation(mode="pandas")
 def tx_type_bucketed_transformation(pdf: pandas.DataFrame):
-    import pandas
+    import pandas as pd
+    df = pd.DataFrame()
     pdf['type_bucketed'] = 'unknown'
     pdf.loc[pdf.type_CASH_IN > 0, 'type_bucketed'] = 'credit'
     pdf.loc[pdf.type_CASH_OUT + pdf.type_DEBIT + pdf.type_PAYMENT > 0, 'type_bucketed'] = 'debit'
     pdf.loc[pdf.type_TRANSFER > 0, 'type_bucketed'] = 'transfer'
-    return pdf
+    df['type_bucketed'] = pdf['type_bucketed']
+    return df
+
+# We combine the results of our two other transformers (type and amount bucketed) here in
+# a third transformer
+@transformation(mode='pandas')
+def merging_dfs(amount_bucketed_df: pandas.DataFrame, type_bucketed_df: pandas.DataFrame):
+    import pandas as pd
+    df = pd.DataFrame()
+    df['amount_bucketed'] = amount_bucketed_df['amount_bucketed']
+    df['type_bucketed'] = type_bucketed_df['type_bucketed']
+    return df
+
 
 # This on-demand feature view runs two different transformations in pipeline mode
 # and returns features drawn from both transformers: amount_bucketed and type_bucketed
 @on_demand_feature_view(
     description="[Online Feature] Bucket transaction amount and type",
-    inputs={"transaction_request": Input(RequestDataSource(request_schema=request_schema))},
+    inputs={"transaction_request": Input(RequestDataSource(request_schema))},
     output_schema=output_schema,
-    mode='pipeline'
+    mode='pipeline',
+    family='fraud',
+    owner='jack@tecton.ai',
+    tags={'release': 'production'}
 )
 def transaction_bucketing(transaction_request: pandas.DataFrame):
-    import pandas as pd
+    # We merge the results of the two transformations in a third transformation
+    return merging_dfs(
+        tx_amount_bucketed_transformation(transaction_request),
+        tx_type_bucketed_transformation(transaction_request))
 
-    df = pd.DataFrame()
-    df['amount_bucketed'] = tx_amount_bucketed_transformation(transaction_request)
-    df['type_bucketed'] = tx_type_bucketed_transformation(transaction_request)
-    return df
