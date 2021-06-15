2b4331165fdebd01e4c3de47237bd42d80a31286
Kevin Stumpf <kevin@tecton.ai>
2021-06-15 12:04:33 -1000

    Remove FV

--- fraud/features/batch_window_counts.py
+++ fraud/features/batch_window_counts.py
@@ -3,28 +3,30 @@ from fraud.entities import user
 from fraud.data_sources.transactions_batch import transactions_batch
 from datetime import datetime
 
-@batch_feature_view(
-    inputs={'transactions': Input(transactions_batch)},
-    entities=[user],
-    mode='spark_sql',
-    online=True,
-    offline=True,
-    feature_start_time=datetime(2021, 5, 20),
-    batch_schedule='1d',
-    ttl='30days',
-    family='fraud',
-    description='Max user transaction amount'
-)
-def max_transaction_amount(transactions):
-    return f'''
-        SELECT
-            last(timestamp) as timestamp,
-            nameorig as user_id,
-            max(amount) as amount
-        FROM
-            {transactions}
-        GROUP BY nameorig
-        '''
+# Note: this is an anti-example
+# This technically works but will create unexpected results because the backfill time range isn't limited
+# @batch_feature_view(
+#     inputs={'transactions': Input(transactions_batch)},
+#     entities=[user],
+#     mode='spark_sql',
+#     online=True,
+#     offline=True,
+#     feature_start_time=datetime(2021, 5, 20),
+#     batch_schedule='1d',
+#     ttl='30days',
+#     family='fraud',
+#     description='Max user transaction amount'
+# )
+# def max_transaction_amount(transactions):
+#     return f'''
+#         SELECT
+#             last(timestamp) as timestamp,
+#             nameorig as user_id,
+#             max(amount) as amount
+#         FROM
+#             {transactions}
+#         GROUP BY nameorig
+#         '''
 
 @batch_feature_view(
     inputs={'transactions': Input(transactions_batch)},
