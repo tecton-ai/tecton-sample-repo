ca10b7b2d4f176ba3065d27fbc486d5036805233
Kevin Stumpf <kevin@tecton.ai>
2021-06-16 23:01:47 -1000

    Comments

--- fraud/features/stream_window_aggregate_feature_views/user_transaction_amount_metrics.py
+++ fraud/features/stream_window_aggregate_feature_views/user_transaction_amount_metrics.py
@@ -6,12 +6,13 @@ from fraud.data_sources.transactions_stream import transactions_stream
 from datetime import datetime
 
 
+# The following defines several sliding time window aggregations over a user's transaction amounts
 @stream_window_aggregate_feature_view(
     inputs={'transactions': Input(transactions_stream)},
     entities=[user],
     mode='spark_sql',
-    aggregation_slide_period='10m',
-    batch_schedule='1d',
+    aggregation_slide_period='10m',  # Defines how frequently feature values get updated in the online store
+    batch_schedule='1d', # Defines how frequently batch jobs are scheduled to ingest into the offline store
     aggregations=[
         FeatureAggregation(column='amount', function='mean', time_windows=['1h', '12h', '24h','72h','168h', '960h']),
         FeatureAggregation(column='amount', function='sum', time_windows=['1h', '12h', '24h','72h','168h', '960h'])
