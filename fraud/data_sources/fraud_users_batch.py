8c7249846fe21acbc7efd7138850c0914a0d2675
Derek Salama <dereksalama@dereks-mbp.lan>
2021-06-18 16:25:44 -0700

    add data source timestamp column  to users hive config

--- fraud/data_sources/fraud_users_batch.py
+++ fraud/data_sources/fraud_users_batch.py
@@ -5,7 +5,8 @@ fraud_users_batch = BatchDataSource(
     name='users_batch',
     batch_ds_config=HiveDSConfig(
         database='demo_fraud',
-        table='fraud_users'
+        table='fraud_users',
+        timestamp_column_name='signup_date'
     ),
     family='fraud',
     owner='matt@tecton.ai',
