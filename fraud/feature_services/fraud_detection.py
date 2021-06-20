32a8135e9567a611e1a0a5df528d22fdfedb424b
Derek Salama <dereksalama@dereks-mbp.lan>
2021-06-20 16:42:23 -0700

    separate out feature services for regular, on demand, continuous

--- fraud/feature_services/fraud_detection.py
+++ fraud/feature_services/fraud_detection.py
@@ -1,11 +1,6 @@
 from tecton import FeatureService
 from fraud.features.stream_window_aggregate_feature_views.user_transaction_amount_metrics import user_transaction_amount_metrics
 from fraud.features.user_transaction_counts import user_transaction_counts
-from fraud.features.transaction_amount_is_high import transaction_amount_is_high
-from fraud.features.transaction_amount_is_higher_than_average import transaction_amount_is_higher_than_average
-from fraud.features.stream_window_aggregate_feature_views.continuous_fraudulent_transactions_count import *
-from fraud.features.stream_window_aggregate_feature_views.continuous_non_fraudulent_transactions_count import *
-from fraud.features.transaction_bucketing import transaction_bucketing
 from fraud.features.stream_feature_views.last_transaction_amount_sql import last_transaction_amount_sql
 # from fraud.features.user_has_good_credit import user_has_good_credit
 
@@ -13,10 +8,7 @@ from fraud.features.stream_feature_views.last_transaction_amount_sql import last
 fraud_detection_feature_service = FeatureService(
     name='fraud_detection_feature_service',
     features=[
-        transaction_amount_is_high,
-        transaction_amount_is_higher_than_average,
         user_transaction_amount_metrics,
-        transaction_bucketing,
         user_transaction_counts,
         last_transaction_amount_sql
     ]
@@ -29,25 +21,9 @@ fraud_detection_feature_service = FeatureService(
 #     family='fraud',
 #     tags={'release': 'production'},
 #     features=[
-#         user_has_good_credit, # New feature
-#         transaction_amount_is_high,
-#         transaction_amount_is_higher_than_average,
-#         user_transaction_amount_metrics,
-#         transaction_bucketing,
-#         user_transaction_counts,
+#        user_has_good_credit, # New feature
+#        user_transaction_amount_metrics,
+#        user_transaction_counts,
+#        last_transaction_amount_sql
 #     ]
 # )
-
-
-continuous_feature_service = FeatureService(
-    name='continuous_feature_service',
-    description='A FeatureService providing continuous features.',
-    family='fraud',
-    tags={'release': 'production'},
-    owner='kevin@tecton.ai',
-    features=[
-        continuous_non_fraudulent_transactions_count,
-        continuous_fraudulent_transactions_count,
-        last_transaction_amount_sql
-    ]
-)
