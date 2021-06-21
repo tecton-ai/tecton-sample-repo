0afffae6781979722c7f5583feddcfdef484a27f
Derek Salama <dereksalama@dereks-mbp.lan>
2021-06-20 17:43:39 -0700

    just remove transaction bucketing feature since it requires a bunch of columns

--- fraud/feature_services/fraud_detection.py
+++ fraud/feature_services/fraud_detection.py
@@ -2,6 +2,8 @@ from tecton import FeatureService
 from fraud.features.stream_window_aggregate_feature_views.user_transaction_amount_metrics import user_transaction_amount_metrics
 from fraud.features.user_transaction_counts import user_transaction_counts
 from fraud.features.stream_feature_views.last_transaction_amount_sql import last_transaction_amount_sql
+from fraud.features.transaction_amount_is_high import transaction_amount_is_high
+from fraud.features.transaction_amount_is_higher_than_average import transaction_amount_is_higher_than_average
 # from fraud.features.user_has_good_credit import user_has_good_credit
 
 
@@ -10,7 +12,9 @@ fraud_detection_feature_service = FeatureService(
     features=[
         user_transaction_amount_metrics,
         user_transaction_counts,
-        last_transaction_amount_sql
+        last_transaction_amount_sql,
+        transaction_amount_is_high,
+        transaction_amount_is_higher_than_average,
     ]
 )
 
