3ab9009b537e0ec95f2ba0d807a81b9079aa82ec
Derek Salama <derek@tecton.ai>
2021-09-23 09:21:15 -0700

    switch to sliding window

--- ads/features/batch_feature_views/user_distinct_ad_count_7d.py
+++ ads/features/batch_feature_views/user_distinct_ad_count_7d.py
@@ -1,7 +1,24 @@
-from tecton import batch_feature_view, Input, materialization_context, DatabricksClusterConfig, BackfillConfig
+from tecton import batch_feature_view, Input, tecton_sliding_window, transformation, const, BackfillConfig
 from ads.entities import user
 from ads.data_sources.ad_impressions import ad_impressions_batch
 from datetime import datetime
+
+# Counts distinct ad ids for each user and window. The timestamp
+# for the feature is the end of the window.
+# window_input_df is created by passing the original input through
+# tecton_sliding_window transformation.
+@transformation(mode='spark_sql')
+def user_distinct_ad_count_transformation(window_input_df):
+    return f'''
+        SELECT
+            user_uuid as user_id,
+            approx_count_distinct(ad_id) as distinct_ad_count,
+            window_end as timestamp
+        FROM
+            {window_input_df}
+        GROUP BY
+            user_uuid, window_end
+        '''
 
 @batch_feature_view(
     inputs={'ad_impressions': Input(ad_impressions_batch, window='7d')},
@@ -11,13 +28,6 @@ from datetime import datetime
     batch_schedule='1d',
     online=False,
     offline=False,
-    batch_cluster_config=DatabricksClusterConfig(
-        instance_type='m5.xlarge',
-        number_of_workers=4,
-        spark_config={
-            'spark.executor.memory': '5000m',
-        },
-    ),
     feature_start_time=datetime(2021, 4, 1),
     backfill_config=BackfillConfig("multiple_batch_schedule_intervals_per_job"),
     family='ad_serving',
@@ -25,16 +35,10 @@ from datetime import datetime
     owner='david@tecton.ai',
     description='How many distinct advertisements a user has been shown in the last week'
 )
-def user_distinct_ad_count_7d(ad_impressions, context=materialization_context()):
-    return f'''
-        SELECT
-            user_uuid as user_id,
-            count(distinct ad_id) as distinct_ad_count,
-            window.end - INTERVAL 1 SECOND as timestamp
-        FROM
-            {ad_impressions}
-        GROUP BY
-            user_uuid, window(timestamp, '7 days', '1 day')
-        HAVING
-            timestamp >= '{context.feature_start_time_string}' AND timestamp < '{context.feature_end_time_string}'
-        '''
+def user_distinct_ad_count_7d(ad_impressions):
+    return user_distinct_ad_count_transformation(
+        # Use tecton_sliding_transformation to create trailing 7 day time windows.
+        # The slide_interval defaults to the batch_schedule (1 day).
+        tecton_sliding_window(ad_impressions,
+            timestamp_key=const('timestamp'),
+            window_size=const('7d')))
