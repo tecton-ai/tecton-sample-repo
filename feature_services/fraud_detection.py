70e2f29a1e5820fd6c3ade769d832cba6b2f5c87
Matt Bleifer <mattbleifer@matts-mbp-2.lan>
2022-05-11 16:24:26 -0700

    Updated Feature Service

--- feature_services/fraud_detection.py
+++ feature_services/fraud_detection.py
@@ -1,44 +1,19 @@
 from tecton import FeatureService
-from fraud.features.stream_window_aggregate_feature_views.user_transaction_amount_metrics import user_transaction_amount_metrics
-from fraud.features.batch_window_aggregate_feature_views.user_transaction_counts import user_transaction_counts
-from fraud.features.stream_feature_views.last_transaction_amount_sql import last_transaction_amount_sql
-from fraud.features.on_demand_feature_views.transaction_amount_is_high import transaction_amount_is_high
-from fraud.features.on_demand_feature_views.transaction_amount_is_higher_than_average import transaction_amount_is_higher_than_average
-from fraud.features.batch_feature_views.user_distinct_merchant_transaction_count_30d import user_distinct_merchant_transaction_count_30d
-from fraud.features.stream_window_aggregate_feature_views.last_transactions import user_recent_transactions
-# from fraud.features.batch_feature_views.user_has_great_credit import user_has_great_credit
+from features.batch_feature_views.user_transaction_counts import user_transaction_counts
+from features.stream_feature_views.user_last_transaction_amount import user_last_transaction_amount
+from features.on_demand_feature_views.transaction_amount_is_high import transaction_amount_is_high
+from features.on_demand_feature_views.transaction_amount_is_higher_than_average import transaction_amount_is_higher_than_average
+from features.batch_feature_views.user_distinct_merchant_transaction_count_30d import user_distinct_merchant_transaction_count_30d
+from fraud.features.batch_feature_views.user_has_good_credit import user_has_good_credit
 
 fraud_detection_feature_service = FeatureService(
     name='fraud_detection_feature_service',
     features=[
-        last_transaction_amount_sql,
+        user_last_transaction_amount,
         transaction_amount_is_high,
         transaction_amount_is_higher_than_average,
-        user_transaction_amount_metrics,
         user_transaction_counts,
-        user_distinct_merchant_transaction_count_30d
-    ]
-)
-
-# fraud_detection_feature_service_v2 = FeatureService(
-#     name='fraud_detection_feature_service:v2',
-#     description='A FeatureService providing features for a model that predicts if a transaction is fraudulent.',
-#     family='fraud',
-#     tags={'release': 'production'},
-#     features=[
-#         user_has_great_credit, # New feature
-#         last_transaction_amount_sql,
-#         transaction_amount_is_high,
-#         transaction_amount_is_higher_than_average,
-#         user_transaction_amount_metrics,
-#         user_transaction_counts,
-#         user_distinct_merchant_transaction_count_30d
-#     ]
-# )
-
-minimal_fs = FeatureService(
-    name='minimal_fs',
-    features=[
-        transaction_amount_is_high
+        user_distinct_merchant_transaction_count_30d,
+        user_has_good_credit
     ]
 )
