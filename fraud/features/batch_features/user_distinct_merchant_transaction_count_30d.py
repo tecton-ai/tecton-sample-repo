c9589801c3820b05e878e2659c80f7cb5cf77810
Jake Noble <jake@tecton.ai>
2022-07-14 18:13:44 -0400

    Add some comments to user_distinct_merchant_transaction_count_30d

--- fraud/features/batch_features/user_distinct_merchant_transaction_count_30d.py
+++ fraud/features/batch_features/user_distinct_merchant_transaction_count_30d.py
@@ -4,6 +4,11 @@ from fraud.data_sources.transactions import transactions_batch
 from datetime import datetime, timedelta
 
 
+# This feature view is an example of a "custom aggregation" using `incremental_backfills=True`. This allows users to
+# compute aggregations over long windows of data using SQL aggregations.
+#
+# See this documentation for more info:
+# https://docs.tecton.ai/latest/overviews/framework/feature_views/batch/incremental_backfills.html.
 @batch_feature_view(
     sources=[FilteredSource(transactions_batch, start_time_offset=timedelta(days=-29))],
     entities=[user],
@@ -22,6 +27,9 @@ def user_distinct_merchant_transaction_count_30d(transactions_batch, context=mat
     return f'''
         SELECT
             user_id,
+            -- The timestamp for the feature data must be in the materialization range: [start_time, end_time). The
+            -- best practice for "custom aggregations" to avoid data leakage is to use the latest timestamp in that
+            -- range, i.e. end_time - 1 microsecond.
             TO_TIMESTAMP("{context.end_time}") - INTERVAL 1 MICROSECOND as timestamp,
             COUNT(DISTINCT merchant) AS distinct_merchant_transaction_count_30d
         FROM {transactions_batch}
