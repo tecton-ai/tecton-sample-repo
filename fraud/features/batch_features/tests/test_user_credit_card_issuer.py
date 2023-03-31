81ecfef66b0e088bef342a4b3e7dff575e21156e
brian-tecton-ai <brian@tecton.ai>
2023-03-31 21:28:44 +0000

    Update unit tests for 0.6 and put behind env var conditional

--- fraud/features/batch_features/tests/test_user_credit_card_issuer.py
+++ fraud/features/batch_features/tests/test_user_credit_card_issuer.py
@@ -1,13 +1,16 @@
+import os
 from datetime import datetime, timedelta
-import pytest
+
 import pandas
+import pytest
+
 from fraud.features.batch_features.user_credit_card_issuer import user_credit_card_issuer
 
 
 # The `tecton_pytest_spark_session` is a PyTest fixture that provides a
 # Tecton-defined PySpark session for testing Spark transformations and feature
 # views.
-@pytest.mark.skip(reason="Requires JDK installation and $JAVA_HOME env variable to run.")
+@pytest.mark.skipif(os.environ.get("TECTON_TEST_SPARK") is None, reason="Requires JDK installation and $JAVA_HOME env variable to run, so we skip unless user sets the `TECTON_TEST_SPARK` env var.")
 def test_user_credit_card_issuer(tecton_pytest_spark_session):
     input_pandas_df = pandas.DataFrame({
         "user_id": ["user_1", "user_2", "user_3", "user_4"],
@@ -18,12 +21,11 @@ def test_user_credit_card_issuer(tecton_pytest_spark_session):
 
     # Simulate materializing features for May 1st.
     output = user_credit_card_issuer.test_run(
-        spark=tecton_pytest_spark_session,
         start_time=datetime(2022, 5, 1),
         end_time=datetime(2022, 5, 2),
         fraud_users_batch=input_spark_df)
 
-    actual = output.toPandas()
+    actual = output.to_pandas()
 
     expected = pandas.DataFrame({
         "user_id": ["user_1", "user_2", "user_3", "user_4"],
--- fraud/features/batch_features/tests/test_user_distinct_merchant_transaction_count_30d.py
+++ fraud/features/batch_features/tests/test_user_distinct_merchant_transaction_count_30d.py
@@ -1,29 +1,36 @@
+import os
 from datetime import datetime, timedelta
-import pytest
+
 import pandas
+import pytest
+
 from fraud.features.batch_features.user_distinct_merchant_transaction_count_30d import user_distinct_merchant_transaction_count_30d
 
 
 # The `tecton_pytest_spark_session` is a PyTest fixture that provides a
 # Tecton-defined PySpark session for testing Spark transformations and feature
 # views.
-@pytest.mark.skip(reason="Requires JDK installation and $JAVA_HOME env variable to run.")
+@pytest.mark.skipif(os.environ.get("TECTON_TEST_SPARK") is None, reason="Requires JDK installation and $JAVA_HOME env variable to run, so we skip unless user sets the `TECTON_TEST_SPARK` env var.")
 def test_user_distinct_merchant_transaction_count_30d(tecton_pytest_spark_session):
+    timestamps = [datetime(2022, 4, 10), datetime(2022, 4, 25), datetime(2022, 5, 1)]
     input_pandas_df = pandas.DataFrame({
         "user_id": ["user_1", "user_1", "user_2"],
         "merchant": ["merchant_1", "merchant_2", "merchant_1"],
-        "timestamp": [datetime(2022, 4, 10), datetime(2022, 4, 25), datetime(2022, 5, 1)],
+        "timestamp": timestamps,
+        "partition_0": [ts.year for ts in timestamps],
+        "partition_1": [ts.month for ts in timestamps],
+        "partition_2": [ts.day for ts in timestamps],
     })
     input_spark_df = tecton_pytest_spark_session.createDataFrame(input_pandas_df)
 
     # Simulate materializing features for May 1st.
     output = user_distinct_merchant_transaction_count_30d.test_run(
-        spark=tecton_pytest_spark_session,
         start_time=datetime(2022, 5, 1),
         end_time=datetime(2022, 5, 2),
-        transactions_batch=input_spark_df)
+        transactions_batch=input_spark_df
+    )
 
-    actual = output.toPandas()
+    actual = output.to_pandas()
 
     expected = pandas.DataFrame({
         "user_id": ["user_1", "user_2"],
