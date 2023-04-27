af043a5a7b9041efda8c3b61d487d4ccd19f88b0
Kevin Zhang <kzhang@tecton.ai>
2023-04-26 21:08:11 -0700

    Fix
    Signed-off-by: Kevin Zhang <kzhang@tecton.ai>

--- ads/features/stream_features/user_clicks_with_transformations.py
+++ ads/features/stream_features/user_clicks_with_transformations.py
@@ -16,20 +16,20 @@ def _get_ds_schema_for_fv():
 
 @stream_feature_view(
     name="keyword_clicks_fv_with_pandas_filter",
-    source=FilteredSource(keyword_click_source),
+    source=FilteredSource(keyword_click_source), # Pandas/Python transformation requires a PushSource.
     entities=[content_keyword],
     online=True,
     offline=True,
-    mode="pandas",
+    mode="pandas", # Use mode pandas to execute pandas code and `python` to execute python code.
     feature_start_time=datetime(2023, 1, 1),
     batch_schedule=timedelta(days=1),
     ttl=timedelta(days=30),
     tags={'release': 'production'},
     owner='pooja@tecton.ai',
     description='The ad clicks for a content keyword',
-    schema=_get_ds_schema_for_fv(),
-    batch_trigger=BatchTriggerType.MANUAL,
-    batch_compute=db_config,
+    schema=_get_ds_schema_for_fv(), # This stream feature view requires an input schema that defines what the data looks like after a transformation is applied.
+    batch_trigger=BatchTriggerType.MANUAL, # Batch materialization must be manual.
+    batch_compute=db_config, # Users will need Databricks 10.4 and up to enable PySpark
 )
 def content_keyword_click_counts_push_pandas(click_event_source):
     click_event_source = click_event_source[click_event_source["clicked"] > 0]
