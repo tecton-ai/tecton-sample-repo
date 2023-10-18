bef007910522e1e4be863a5a0cb6a02ee8ed06ad
Mihir Mathur <11710043+Mihirmathur@users.noreply.github.com>
2023-10-18 10:36:06 -0700

    Initialize recsys example (#63)

---
+++ recsys/data_sources/session_events.py
@@ -0,0 +1,22 @@
+from tecton import spark_batch_config, BatchSource
+
+@spark_batch_config()
+def sessions_data_source_function(spark):
+    from pyspark.sql.types import StructField, StructType, IntegerType, TimestampType, StringType
+    schema = StructType([
+        StructField("session", IntegerType(), True),
+        StructField("aid", IntegerType(), True),
+        StructField("ts", TimestampType(), True),
+        StructField("type", StringType(), True)
+    ])
+
+    df = spark.read.schema(schema).parquet("s3://tecton.ai.public/recsys-demo/train.parquet")
+    return df
+
+sessions_batch = BatchSource(
+    name="sessions_batch",
+    batch_config=sessions_data_source_function,
+    owner='mihir@tecton.ai',
+    tags={'release': 'production'}
+)
+
