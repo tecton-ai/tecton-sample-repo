babfe9e6ff08d8551ace06892ae9396ab96d6384
Mahesh Murag <maheshmmurag@gmail.com>
2023-10-19 13:12:07 -0400

    Modify S3 URI for recsys-demo (#65)

--- recsys/data_sources/session_events.py
+++ recsys/data_sources/session_events.py
@@ -10,7 +10,7 @@ def sessions_data_source_function(spark):
         StructField("type", StringType(), True)
     ])
 
-    df = spark.read.schema(schema).parquet("s3://tecton.ai.public/recsys-demo/train.parquet")
+    df = spark.read.schema(schema).parquet("s3://tecton.ai.public/recsys-demo/tutorials/train.parquet")
     return df
 
 sessions_batch = BatchSource(
