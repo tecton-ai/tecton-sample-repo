c23cea5d79f6c941d85d9c30e6486d1dcabc0c1c
Mihir Mathur <mihir@tecton.ai>
2023-10-19 15:25:11 -0700

    comment out secondary key aggregate example

--- recsys/features/batch_features/article_features.py
+++ recsys/features/batch_features/article_features.py
@@ -22,21 +22,21 @@ def article_sessions(sessions_batch):
         FROM {sessions_batch}
         """
 
-
-@batch_feature_view(
-    description="Article interactions: clicks, carts, orders on an article",
-    sources=[sessions_batch],
-    aggregation_secondary_key="type",
-    entities=[article],
-    mode="spark_sql",
-    timestamp_field="ts",
-    aggregation_interval=timedelta(days=1),
-    aggregations=[
-        Aggregation(function="count", column="interaction", time_window=timedelta(days=30)),
-    ],
-)
-def article_interactions(sessions_batch):
-    return f"""
-        SELECT aid, ts, type, 1 as interaction
-        FROM {sessions_batch}
-        """
+# Secondary Key Aggregate example - available in 0.8
+# @batch_feature_view(
+#     description="Article interactions: clicks, carts, orders on an article",
+#     sources=[sessions_batch],
+#     aggregation_secondary_key="type",
+#     entities=[article],
+#     mode="spark_sql",
+#     timestamp_field="ts",
+#     aggregation_interval=timedelta(days=1),
+#     aggregations=[
+#         Aggregation(function="count", column="interaction", time_window=timedelta(days=30)),
+#     ],
+# )
+# def article_interactions(sessions_batch):
+#     return f"""
+#         SELECT aid, ts, type, 1 as interaction
+#         FROM {sessions_batch}
+#         """
