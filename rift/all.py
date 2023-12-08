84d1eb10b71c2b17bc179deb92bab7922dd6a09d
Zhou Tan <zhou@tecton.ai>
2023-12-08 14:40:55 -0800

    feat: add rift smoke test feature repo

---
+++ rift/all.py
@@ -0,0 +1,52 @@
+from datetime import *
+import tecton
+import pandas
+
+entity = tecton.Entity(name="user", join_keys=["USER_ID"])
+
+schema = [
+    tecton.types.Field("USER_ID", tecton.types.Int64),
+    tecton.types.Field("TIMESTAMP", tecton.types.Timestamp),
+    tecton.types.Field("VALUE", tecton.types.Int64),
+]
+
+start = datetime(2023,10,20,0,0,0)
+end = datetime(2023,10,23,0,0,0)
+
+@tecton.pandas_batch_config()
+def backfill_data():
+    start = datetime(2023,10,20,0,0,0)
+    end = datetime(2023,10,23,0,0,0)
+    return pandas.DataFrame({
+        "USER_ID": [1,2,3],
+        "TIMESTAMP": [
+            start + timedelta(days=1),
+            start + timedelta(days=1, hours=1),
+            start + timedelta(days=1, hours=2),
+        ],
+        "VALUE": [1,2,3],
+        })
+
+
+source = tecton.PushSource(
+        name = "s",
+        schema = schema,
+        batch_config=backfill_data,
+)
+
+
+bfv = tecton.StreamFeatureView(
+    name="bfv",
+    mode="pandas",
+    source=source,
+    entities=[entity],
+    batch_schedule=timedelta(days=1),
+    feature_start_time=start,
+    manual_trigger_backfill_end_time=end,
+    timestamp_field="TIMESTAMP",
+    offline=True,
+    online=True,
+    offline_store=tecton.DeltaConfig(),
+    ttl=timedelta(days=365),
+    schema=schema,
+)
