ea005d6c7704e42259f2590a5b5bf5eec34b4c19
Pooja Bhat <poojabhat@Poojas-MacBook-Pro.local>
2023-12-14 17:21:08 -0500

    fix

--- fraud/features/stream_features/user_transaction_amount_metrics_no_batch.py
+++ fraud/features/stream_features/user_transaction_amount_metrics_no_batch.py
@@ -1,6 +1,6 @@
 from datetime import datetime, timedelta
 
-from tecton import stream_feature_view, Aggregation, ParquetConfig
+from tecton import stream_feature_view, Aggregation, DeltaConfig
 from tecton.types import Field, Int64, String, Timestamp, Float64
 
 from fraud.data_sources.transactions import ingest_source_no_batch
@@ -18,7 +18,7 @@ schema = [
     entities=[user],
     online=True,
     offline=True,
-    offline_store=ParquetConfig(),
+    offline_store=DeltaConfig(),
     feature_start_time=datetime(2023, 1, 1),
     aggregations=[
         Aggregation(column='amt', function='sum', time_window=timedelta(hours=1)),
