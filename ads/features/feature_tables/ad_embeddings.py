e64fd4971fc59b061f2cfeb7bd2c9d497855abc8
Lilly <lilly.gray@tecton.ai>
2024-08-29 16:22:14 -0700

    upgrade

--- ads/features/feature_tables/ad_embeddings.py
+++ ads/features/feature_tables/ad_embeddings.py
@@ -1,4 +1,5 @@
-from tecton import Attribute, FeatureTable
+from tecton import Attribute, Entity
+from tecton import FeatureTable
 from tecton.types import Field, String, Timestamp, Array, Float64
 from ads.entities import ad
 from datetime import timedelta
@@ -8,11 +9,18 @@ schema = [
     Field('timestamp', Timestamp),
     Field('ad_embedding', Array(Float64))
 ]
-
+#
+ad2 = Entity(
+    name='ad2',
+    join_keys=[Field("ad_id", String)],
+    description='An ad',
+    owner='demo-user@tecton.ai',
+    tags={'release': 'production'}
+)
 
 ad_embeddings = FeatureTable(
     name='ad_embeddings',
-    entities=[ad],
+    entities=[ad2],
     features=[Attribute("ad_embedding", dtype=Array(Float64))],
     online=True,
     offline=True,
