85dee26b2677bec2c9077779f1e4ea098888bf2f
Kevin Stumpf <kevin@tecton.ai>
2025-02-13 13:06:15 -0500

    Added more feature examples (#84)

---
+++ spark/fraud/features/batch_features/user_time_window_series_aggregation_features.py
@@ -0,0 +1,32 @@
+from tecton import batch_feature_view, Aggregate, TimeWindowSeries
+from datetime import timedelta
+from fraud.data_sources.transactions import transactions_batch
+from fraud.entities import user
+from tecton.types import Field, Int32
+
+@batch_feature_view(
+    sources=[transactions_batch],
+    mode="spark_sql",
+    entities=[user],
+    aggregation_interval=timedelta(days=1),
+    description="Daily User transaction sums over the past 7 days",
+    timestamp_field="timestamp",
+    features=[
+        Aggregate(
+            input_column=Field("value", Int32),
+            function="sum",
+            time_window=TimeWindowSeries(
+                series_start=timedelta(days=-7),
+                window_size=timedelta(days=1),
+            ),
+            description="An array of sums of transactions for every day over the past 7 days"
+        )
+    ],
+)
+def user_transaction_sums(transactions):
+    return f"""
+        SELECT user_id, timestamp, value
+        FROM {transactions}
+        """
+
+
