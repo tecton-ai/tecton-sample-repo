6622c950ce8f49392736ffe8e995f9e794cf7dc0
Lee Henderson <lee.henderson@tecton.ai>
2025-07-28 20:25:15 -0700

    remove custom spark session to fix tests

--- spark/fraud/features/stream_features/tests/test_user_recent_transactions.py
+++ spark/fraud/features/stream_features/tests/test_user_recent_transactions.py
@@ -15,7 +15,7 @@ from pyspark.sql import SparkSession
 # Spark transformations and feature views. This session can be configured as needed by the user. If an entirely new
 # session is needed, then you can create your own and set it with `tecton.set_tecton_spark_session()`.
 @pytest.mark.skipif(os.environ.get("TECTON_TEST_SPARK") is None, reason="Requires JDK installation and $JAVA_HOME env variable to run, so we skip unless user sets the `TECTON_TEST_SPARK` env var.")
-def test_user_recent_transactions(my_custom_spark_session):
+def test_user_recent_transactions(tecton_pytest_spark_session):
     data = [
         ("user_1", datetime(2022, 5, 1), 100, "2022", "05", "01"),
         ("user_1", datetime(2022, 5, 1), 200, "2022", "05", "01"),
@@ -25,7 +25,7 @@ def test_user_recent_transactions(my_custom_spark_session):
 
     schema = ["user_id", "timestamp", "amt", "partition_0", "partition_1", "partition_2"]
 
-    input_spark_df = my_custom_spark_session.createDataFrame(data, schema)
+    input_spark_df = tecton_pytest_spark_session.createDataFrame(data, schema)
 
     # Simulate materializing features for May 1st.
     output = user_recent_transactions.test_run(
@@ -47,26 +47,8 @@ def test_user_recent_transactions(my_custom_spark_session):
 
     pandas.testing.assert_frame_equal(actual, expected,  check_like=True)
 
-
-# Example showing of how to create your own spark session for testing instead of the Tecton provided
-# tecton_pytest_spark_session.
-@pytest.fixture(scope='function')
-def my_custom_spark_session():
-    """Returns a custom spark session configured for use in Tecton unit testing."""
-    with resources.path("tecton_spark.jars", "tecton-udfs-spark-3.jar") as path:
-        tecton_udf_jar_path = str(path)
-
-    spark = (
-        SparkSession.builder.appName("my_custom_spark_session")
-        .config("spark.jars", tecton_udf_jar_path)
-        # This short-circuit's Spark's attempt to auto-detect a hostname for the master address, which can lead to
-        # errors on hosts with "unusual" hostnames that Spark believes are invalid.
-        .config("spark.driver.host", "localhost")
-        .config("spark.sql.session.timeZone", "UTC")
-        .getOrCreate()
-    )
-    try:
-        tecton.set_tecton_spark_session(spark)
-        yield spark
-    finally:
-        spark.stop()
+@pytest.fixture(scope='module', autouse=True)
+def configure_spark_session(tecton_pytest_spark_session):
+    # Custom configuration for the spark session. In this case, configure the timezone so that we don't need to specify
+    # a timezone for every datetime in the mock data.
+    tecton_pytest_spark_session.conf.set("spark.sql.session.timeZone", "UTC")
